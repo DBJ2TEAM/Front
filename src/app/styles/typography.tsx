509369064354ddbc22e18937299526776dcd8304
김진우 <jinwoo@gimjin-uui-MacBookPro.local>
2023-11-12 13:46:51 +0900

    [Design] Typography

---
+++ src/app/styles/typography.tsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+export const Heading1 = styled.h1`
+  font-size: 48px;
+  line-height: auto;
+  font-weight: 500;
+`
+
+export const Heading2 = styled.h2`
+  font-size: 32px;
+  line-height: auto;
+  font-weight: 500;
+`
+
+export const Title = styled.h1`
+  font-size: 26px;
+  line-height: auto;
+  font-weight: 600;
+`
+
+export const Text = styled.p`
+  font-size: 16px;
+  line-height: 160%;
+  font-weight: 500;
+`
+
+export const BigText = styled.p`
+  font-size: 18px;
+  line-height: 160%;
+  font-weight: 500;
+`
+
+export const Small = styled.p`
+  font-size: 14px;
+  line-height: auto;
+  font-weight: 500;
+`
+
+export const Caption = styled.p`
+  font-size: 18px;
+  line-height: auto;
+  font-weight: 700;
+`
