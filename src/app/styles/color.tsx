c9e86a75c6448dc7389a9abf34ba73e1ea2c9217
김진우 <jinwoo@gimjin-uui-MacBookPro.local>
2023-11-12 13:57:10 +0900

    [Design] typography 수정, color 추가

---
+++ src/app/styles/color.tsx
@@ -0,0 +1,5 @@
+export const primaryColor = '1F2B6C'
+export const secondaryColor = '159EEC'
+export const accent = 'BFD2F8'
+export const black = '212124'
+export const white = 'FCFEFE'
--- src/app/styles/typography.tsx
+++ src/app/styles/typography.tsx
@@ -1,43 +1,20 @@
-import styled from 'styled-components'
+export const fontSizes = {
+  heading1: '48px',
+  heading2: '32px',
+  title: '26px',
+  text: '16px',
+  bigText: '18px',
+  small: '14px',
+  caption: '18px',
+}
 
-export const Heading1 = styled.h1`
-  font-size: 48px;
-  line-height: auto;
-  font-weight: 500;
-`
+export const lineHeights = {
+  normal: 'auto',
+  double: '160%',
+}
 
-export const Heading2 = styled.h2`
-  font-size: 32px;
-  line-height: auto;
-  font-weight: 500;
-`
-
-export const Title = styled.h1`
-  font-size: 26px;
-  line-height: auto;
-  font-weight: 600;
-`
-
-export const Text = styled.p`
-  font-size: 16px;
-  line-height: 160%;
-  font-weight: 500;
-`
-
-export const BigText = styled.p`
-  font-size: 18px;
-  line-height: 160%;
-  font-weight: 500;
-`
-
-export const Small = styled.p`
-  font-size: 14px;
-  line-height: auto;
-  font-weight: 500;
-`
-
-export const Caption = styled.p`
-  font-size: 18px;
-  line-height: auto;
-  font-weight: 700;
-`
+export const fontWeights = {
+  regular: 400,
+  medium: 500,
+  bold: 700,
+}
